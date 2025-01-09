76193087a56f58ef84b17c692d354028688a0a05
albertopiodematteis <a.dematteis15@studenti.uniba.it>
2025-01-09 16:59:34 +0100

    Update coco_eval.py inserted a return for summarize()

--- datasets/coco_eval.py
+++ datasets/coco_eval.py
@@ -63,8 +63,11 @@ class CocoEvaluator(object):
 
     def summarize(self):
         for iou_type, coco_eval in self.coco_eval.items():
-            print("IoU metricA: {}".format(iou_type))
+            evaluation += "IoU metric: {}".format(iou_type)
+            evaluation += "\n"
+            print("IoU metric: {}".format(iou_type))
             coco_eval.summarize()
+        return evaluation
 
     def prepare(self, predictions, iou_type):
         if iou_type == "bbox":
